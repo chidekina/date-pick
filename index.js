c25b7cc5f9cc7725ee7f1cf64a13f2a57fa1395a
CESAR HIDEKI NAGANO <cesar0610@digitalcollegesul.local>
2024-10-16 11:57:22 -0300

    project done

---
+++ index.js
@@ -0,0 +1,42 @@
+const monthsName = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+let monthSelected = month.value;
+let yearSelected = year.value;
+let daysqt = 0;
+
+
+for (let i = 2024; i >= 1924; i--){
+    year.innerHTML += `<option>${i}</option>`;
+}
+
+for(let i = 0; i < monthsName.length; i++){
+    month.innerHTML += `<option value="${i + 1}">${monthsName[i]}</option>`;
+}
+
+for(let i = 1; i <= 31; i++){
+    day.innerHTML += `<option>${i}</option>`;
+}
+
+month.onchange = () => {
+
+    if(monthSelected == 1 || monthSelected == 3 || monthSelected == 5 || monthSelected == 7 || monthSelected == 8 || monthSelected == 10 || monthSelected == 12) daysqt = 31;
+    else if (monthSelected == 4 || monthSelected == 6 || monthSelected == 9 || monthSelected == 11) daysqt = 30;
+    else{
+        if(yearSelected % 400 == 0 || yearSelected % 4 == 0 && yearSelected % 100 != 0) daysqt = 29;
+        else daysqt = 28;
+    } 
+    
+    day.innerHTML = '';
+
+    for(let i = 1; i <= daysqt; i++){
+        day.innerHTML += `<option>${i}</option>`;
+    }
+}
+
+year.onchange = () => {
+    console.log(monthSelected)
+    if (monthSelected == 2){
+        if(yearSelected % 400 == 0 || yearSelected % 4 == 0 && yearSelected % 100 != 0) daysqt = 29;
+        else daysqt = 28;
+    }
+}
+
